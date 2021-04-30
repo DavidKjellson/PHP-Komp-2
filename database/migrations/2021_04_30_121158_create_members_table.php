c927a4d4cf90ac0bd0eca5bf9dbd0d93d16069b8
David Kjellson <david.kjellson@gmail.com>
2021-04-30 14:18:12 +0200

    Member migration

---
+++ database/migrations/2021_04_30_121158_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id('member_id');
+            $table->timestamps();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('birthday');
+            $table->string('email');
+            $table->integer('member_fee');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
